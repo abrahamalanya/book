170bc1977d5fbfdada51969de78013a22d472755
abrahamalanya <alanyaoficial@gmail.com>
2021-09-15 23:31:15 -0500

    iniciando historia

---
+++ script.js
@@ -0,0 +1,23 @@
+var header_title = new Vue({
+    el: '#header_title',
+    data: {
+        title: 'Historia'
+    }
+});
+
+var data_book = new Vue({
+    el: '#data_book',
+    data: {
+        stories: [
+            {
+                content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut fugiat impedit ut odio labore, nesciunt dolorum, eum, omnis nobis atque quo consequatur quae iste eveniet adipisci consectetur! Aperiam, impedit perspiciatis?'
+            },
+            {
+                content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut fugiat impedit ut odio labore, nesciunt dolorum, eum, omnis nobis atque quo consequatur quae iste eveniet adipisci consectetur! Aperiam, impedit perspiciatis?'
+            },
+            {
+                content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut fugiat impedit ut odio labore, nesciunt dolorum, eum, omnis nobis atque quo consequatur quae iste eveniet adipisci consectetur! Aperiam, impedit perspiciatis?'
+            }
+        ]
+    }
+});
