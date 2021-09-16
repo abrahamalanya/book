194a8585f970b5f44fc02493fafd034e0cc16e1d
abrahamalanya <alanyaoficial@gmail.com>
2021-09-16 14:14:21 -0500

    agregando cartas

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var header_title = new Vue({
     el: '#header_title',
     data: {
-        title: 'Historia'
+        title: 'Top 10'
     }
 });
 
@@ -10,13 +10,59 @@ var data_book = new Vue({
     data: {
         stories: [
             {
-                content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut fugiat impedit ut odio labore, nesciunt dolorum, eum, omnis nobis atque quo consequatur quae iste eveniet adipisci consectetur! Aperiam, impedit perspiciatis?'
+                title: 'Nro. 01',
+                image: "https://picsum.photos/id/10/430?grayscale&blur=1",
+                content: '01'
             },
             {
-                content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut fugiat impedit ut odio labore, nesciunt dolorum, eum, omnis nobis atque quo consequatur quae iste eveniet adipisci consectetur! Aperiam, impedit perspiciatis?'
+                title: 'Nro. 02',
+                image: "https://picsum.photos/id/101/430?grayscale&blur=1",
+                content: '02'
             },
             {
-                content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut fugiat impedit ut odio labore, nesciunt dolorum, eum, omnis nobis atque quo consequatur quae iste eveniet adipisci consectetur! Aperiam, impedit perspiciatis?'
+                title: 'Nro. 03',
+                image: "https://picsum.photos/id/1015/430?grayscale&blur=1",
+                content: '03'
+            },
+            {
+                title: 'Nro. 04',
+                image: "https://picsum.photos/id/1016/430?grayscale&blur=1",
+                content: '04'
+            },
+            {
+                title: 'Nro. 05',
+                image: "https://picsum.photos/id/1018/430?grayscale&blur=1",
+                content: '05'
+            },
+            {
+                title: 'Nro. 06',
+                image: "https://picsum.photos/id/1019/430?grayscale&blur=1",
+                content: '06'
+            },
+            {
+                title: 'Nro. 07',
+                image: "https://picsum.photos/id/1021/430?grayscale&blur=1",
+                content: '07'
+            },
+            {
+                title: 'Nro. 08',
+                image: "https://picsum.photos/id/1028/430?grayscale&blur=1",
+                content: '08'
+            },
+            {
+                title: 'Nro. 09',
+                image: "https://picsum.photos/id/1038/430?grayscale&blur=1",
+                content: '09'
+            },
+            {
+                title: 'Nro. 10',
+                image: "https://picsum.photos/id/1037/430?grayscale&blur=1",
+                content: '10'
+            },
+            {
+                title: 'Nro. 11',
+                image: "https://picsum.photos/id/1041/430?grayscale&blur=1",
+                content: '11'
             }
         ]
     }
